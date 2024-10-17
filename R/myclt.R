dfb4a3c47762355e303b4b44dc8633cbaa65c426
Encho Genchev <esgenchev@gmail.com>
2024-10-17 16:40:32 -0500

    myclt fixed

--- R/myclt.R
+++ R/myclt.R
@@ -8,7 +8,7 @@
 #' @param a lower bound of the uniform distribution
 #' @param b upper bound of the uniform distribution
 #'
-#' @return vector containing the sum of the `n` uniform random variables for each iteration and a histogram
+#' @return histogram containing the sum of the `n` uniform random variables for each iteration
 #'
 #' @examples
 #' myclt(n = 10, iter = 1000, a = 0, b = 5)
@@ -21,5 +21,4 @@ myclt=function(n,iter,a=0,b=5){
   h=hist(sm,plot=FALSE)
   hist(sm,col=rainbow(length(h$mids)),freq=FALSE,main="Distribution of the sum of uniforms")
   curve(dnorm(x,mean=n*(a+b)/2,sd=sqrt(n*(b-a)^2/12)),add=TRUE,lwd=2,col="Blue")
-  sm
 }
